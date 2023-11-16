07248a7a463cf45501aebe2cfa4732cf7a3a137d
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-11-16 23:15:10 +0100

    Unit Test: fix bug on unit test using pest issues

--- tests/Feature/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use BlakvGhost\PHPValidator\Rules\EmailRule;
-use BlakvGhost\PHPValidator\Rules\StringRule;
-
-it('validates string rule successfully')
-    ->does(fn () => (new EmailRule([]))->passes('name', 'John', []))
-    ->assertTrue();
---
+++ tests/Feature/ValidatorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use BlakvGhost\PHPValidator\Validator;
+use BlakvGhost\PHPValidator\ValidatorException;
+use BlakvGhost\PHPValidator\LangManager;
+
+it('throws exception if data is empty', function () {
+    expect(fn () => new Validator([], ['name' => 'string']))
+        ->toThrow(ValidatorException::class)
+        ->withMessage(LangManager::getTranslation('validation.empty_data'));
+});
+
+it('throws exception if rules are empty', function () {
+    expect(fn () => new Validator(['name' => 'John'], []))
+        ->toThrow(ValidatorException::class)
+        ->withMessage(LangManager::getTranslation('validation.empty_rules'));
+});
+
+it('throws exception if rule not found', function () {
+    expect(fn () => new Validator(['name' => 'John'], ['name' => 'nonexistent']))
+        ->toThrow(ValidatorException::class)
+        ->withMessage(LangManager::getTranslation('validation.rule_not_found', ['ruleName' => 'nonexistent']));
+});
+
+it('validates string rule successfully', function () {
+    $validator = new Validator(['name' => 'John'], ['name' => 'string']);
+    expect($validator->isValid())->toBeTrue();
+});
+
+it('throws exception for invalid string rule', function () {
+    expect(fn () => new Validator(['name' => 123], ['name' => 'string']))
+        ->toThrow(ValidatorException::class)
+        ->withMessage(LangManager::getTranslation('validation.string_rule', ['attribute' => 'name']));
+});
