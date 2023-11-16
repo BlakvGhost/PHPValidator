95de1162a888297e9f1fbbe57a28924ad665f836
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-11-16 01:40:26 +0100

    Refactoring the validation class by separating the function from exceptions

--- src/Validator.php
+++ src/Validator.php
@@ -2,17 +2,31 @@
 
 namespace BlakvGhost\PHPValidator;
 
+use BlakvGhost\PHPValidator\ValidatorException;
+
 class Validator
 {
     protected $errors = [];
 
-    public function __construct(protected array $data, protected array $rules)
+    public function __construct(private array $data, protected array $rules)
     {
+        $this->validateConstructorInputs();
         $this->validate();
     }
-
-    public function validate()
+    
+    public static function validate()
     {
         // TODO
+    }
+
+    private function validateConstructorInputs()
+    {
+        if (!isset($this->$data)) {
+            throw new ValidatorException("Les données de validation ne peuvent pas être vide");
+        }
+
+        if (!isset($this->$rules) || count($this->rules) < 1) {
+            throw new ValidatorException("Les règles de validation ne peuvent pas être vide");
+        }
     }
 }
